095df2a8becdc315c5793a9ffaeefd4c5498a4b3
Stu Noon <stoonoon@gmail.com>
2020-01-22 20:42:40 +0000

    added comments

--- 01_includes.ino
+++ 01_includes.ino
@@ -1,6 +1,4 @@
-#include <MIDI.h>
-#include "Nextion.h"
+#include <MIDI.h>  // https://github.com/FortySevenEffects/arduino_midi_library
+#include "Nextion.h" // https://github.com/itead/ITEADLIB_Arduino_Nextion
 #define BOUNCE_WITH_PROMPT_DETECTION  // activates more responsive debounce method - must be defined before Bounce2 include
-#include <Bounce2.h>
-
-
+#include <Bounce2.h> // https://github.com/thomasfredericks/Bounce2
