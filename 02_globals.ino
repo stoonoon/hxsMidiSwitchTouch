69d44d45c5d362192bbbeb839f9791c0612f614c
Stu Noon <stoonoon@gmail.com>
2020-01-22 20:44:10 +0000

    updated pins to match shield

--- 02_globals.ino
+++ 02_globals.ino
@@ -1,6 +1,6 @@
 //EXP pedal and toeswitch
-const int analogExpressionPedalPin= 0; //pin connected to wiper of Expression pedal potentiometer
-const int expToeswitchPin= 12; // pin connected to expression pedal toeswitch
+const int analogExpressionPedalPin= A1; //pin connected to wiper of Expression pedal potentiometer
+const int expToeswitchPin= A0; // pin connected to expression pedal toeswitch
 const int Vin= 5; // comparison voltage for expression pedal voltage divider
 const float R1= 100000; // known resistor value for expression pedal voltage divider
 enum EXPpedalOutput {BOTH, EXP1, EXP2};
@@ -12,7 +12,7 @@ const bool WAIT_FOR_LONG_PRESS = true; // if true, prevents a long press on a sw
 const int longPressMillis = 1000; //how long a single switch must be held down for to trigger a long press event
 const int doubleClickMillis = 500; //maximum time between consecutive clicks on a switch to trigger a double click event
 const int footSwitchCount = 8; // number of footswitches used
-const int footSwitchPin[] = {9,8,7,6,5,4,3,2}; // pins for footswitch inputs
+const int footSwitchPin[] = {0,1,2,3,7,6,5,4}; // pins for footswitch inputs
 const bool footSwitchDown = LOW; //because we are using pullup resistor and switch to GND
 const bool footSwitchUp = !footSwitchDown;
 const int individualActionTypeCount = 3; // number of types in above enum
@@ -29,4 +29,3 @@ const int labelLinesPerSwitch =2;
 const int totalPresetPages = 6; // number of preset pages we are configuring
 
 int currentPage=0; // menu preset page to load on boot
-
